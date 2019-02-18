649712960e47b9df54e0f1dbab892920bad86745
mike evans <evansmj@dukes.jmu.edu>
2019-02-18 10:48:28 -0500

    Use Firestore.

--- app/src/main/java/com/willowtreeapps/capwic_android/MainActivity.kt
+++ app/src/main/java/com/willowtreeapps/capwic_android/MainActivity.kt
@@ -2,11 +2,25 @@ package com.willowtreeapps.capwic_android
 
 import android.app.Activity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : Activity() {
+
+    lateinit var firestore: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.firestore = FirebaseFirestore.getInstance()
+
+        sendHelloWorldToFirestore()
+    }
+
+    private fun sendHelloWorldToFirestore() {
+        this.firestore
+            .collection("eventdata")
+            .document("1337")
+            .set(TestData("Hello firebase i am an app"))
     }
 }
